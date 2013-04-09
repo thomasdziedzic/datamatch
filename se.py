0af298c0a2b51a088901a9552dac7a4c5b28b9f2
Thomas Dziedzic <gostrc@gmail.com>
2013-04-08 21:13:19 -0700

    write all the items that had no matches at all to some files

--- se.py
+++ se.py
@@ -75,7 +75,7 @@ print(len(matchings))
 
 out_csv = []
 empty_row = [''] * num_columns
-with open('matchings2.csv', 'w', newline='') as f:
+with open('matchings.csv', 'w', newline='') as f:
     matchings_writer = csv.writer(f)
     
     for i, i_matchings in matchings.items():
@@ -88,3 +88,19 @@ with open('matchings2.csv', 'w', newline='') as f:
         
     
     matchings_writer.writerows(out_csv)
+
+with open('nomatches1.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    
+    remaining_items1 = set(range(len(reader1))).difference(total_matched_items1)
+    
+    for remaining_item1 in remaining_items1:
+        writer.writerow(reader1[remaining_item1])
+
+with open('nomatches2.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    
+    remaining_items2 = set(range(len(reader2))).difference(total_matched_items2)
+    
+    for remaining_item2 in remaining_items2:
+        writer.writerow(reader2[remaining_item2])
