b736c1f33e8c039fb332cb1ba7ed69603faeabb7
Thomas Dziedzic <gostrc@gmail.com>
2013-05-07 23:47:27 -0700

    remove debug print and add more text to describe the number of matches we print

--- se.py
+++ se.py
@@ -9,10 +9,9 @@ contents2 = utilities.lowerCSV(utilities.readCSV('2.csv'))
 
 matchings, total_matched_items1, total_matched_items2 = utilities.match_data(contents1, contents2, MIN_COLUMNS_TO_MATCH)
 
-print(matchings)
-print(len(matchings))
+print(len(matchings), 'match(es) have been found!')
 
-utilities.write_matches_csv('machings.csv', matchings, contents1, contents2)
+utilities.write_matches_csv('matchings.csv', matchings, contents1, contents2)
 
 utilities.write_no_matches_csv('nomatches1.csv', contents1, total_matched_items1)
 utilities.write_no_matches_csv('nomatches2.csv', contents2, total_matched_items2)
