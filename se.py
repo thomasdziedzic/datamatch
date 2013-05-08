4558a5c6ad0428c21aa854d313021db70e7e8b8a
Thomas Dziedzic <gostrc@gmail.com>
2013-05-07 22:56:09 -0700

    extract matching code into a utility method

--- se.py
+++ se.py
@@ -1,74 +1,17 @@
 #!/usr/bin/python3
 
 import csv
-from itertools import chain, combinations, groupby
-from collections import defaultdict
 
 import utilities
 
 MIN_COLUMNS_TO_MATCH = 2
 
-matchings = defaultdict(list)
-
-def powerset(s):
-    p = list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
-    p.reverse()
-    # we only want a match if there are 2 or more column matches
-    return filter(lambda x: len(x) >= MIN_COLUMNS_TO_MATCH, p)
-
-num_columns = 0
-with open('1.csv', newline='') as c1:
-    # do we really have to do it this way?
-    num_columns = len(csv.DictReader(c1).fieldnames)
-
 reader1 = utilities.lowerCSV(utilities.readCSV('1.csv'))
 reader2 = utilities.lowerCSV(utilities.readCSV('2.csv'))
 
-# need to reverse order and remove []
-column_lists = powerset(list(range(num_columns)))
+num_columns = len(reader1[0])
 
-total_matched_items1 = set()
-total_matched_items2 = set()
-
-for key, column_lists in groupby(column_lists, len):
-    # store these since we want multiple matches for the same column lengths
-    temp_matched_items1 = set()
-    temp_matched_items2 = set()
-    
-    remaining_items1 = set(range(len(reader1))).difference(total_matched_items1)
-    remaining_items2 = set(range(len(reader2))).difference(total_matched_items2)
-    
-    for column_list in column_lists:
-        print(column_list)
-
-        for i in remaining_items1:
-            c1_row = reader1[i]
-
-            relevant_col1 = [c1_row[r] for r in column_list]
-
-            if not all(relevant_col1):
-                continue
-
-            matchings_for_i = []
-
-            for j in remaining_items2:
-                c2_row = reader2[j]
-
-                relevant_col2 = [c2_row[r] for r in column_list]
-                
-                if not all(relevant_col2):
-                    continue
-
-                if relevant_col1 == relevant_col2:    
-                    matchings_for_i.append(j)
-
-            if len(matchings_for_i):
-                matchings[i] += matchings_for_i
-                temp_matched_items1.add(i)
-                temp_matched_items2.update(matchings_for_i)
-                
-    total_matched_items1.update(temp_matched_items1)
-    total_matched_items2.update(temp_matched_items2)
+matchings, total_matched_items1, total_matched_items2 = utilities.match_data(reader1, reader2, MIN_COLUMNS_TO_MATCH)
 
 print(matchings)
 print(len(matchings))
--- utilities.py
+++ utilities.py
@@ -1,4 +1,6 @@
 import csv
+from itertools import chain, combinations, groupby
+from collections import defaultdict
 
 def readCSV(filename):
     with open(filename, newline='') as f:
@@ -9,3 +11,62 @@ def readCSV(filename):
 
 def lowerCSV(contents):
     return [list(map(lambda x: x.lower(), row)) for row in contents]
+
+def powerset(s, min_columns):
+    p = list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+    p.reverse()
+    # we only want a match if there are 2 or more column matches
+    return filter(lambda x: len(x) >= min_columns, p)
+
+def match_data(contents1, contents2, min_columns):
+    matchings = defaultdict(list)
+
+    num_columns = len(contents1[0])
+
+    # need to reverse order and remove []
+    column_lists = powerset(list(range(num_columns)), min_columns)
+
+    total_matched_items1 = set()
+    total_matched_items2 = set()
+
+    for key, column_lists in groupby(column_lists, len):
+        # store these since we want multiple matches for the same column lengths
+        temp_matched_items1 = set()
+        temp_matched_items2 = set()
+        
+        remaining_items1 = set(range(len(contents1))).difference(total_matched_items1)
+        remaining_items2 = set(range(len(contents2))).difference(total_matched_items2)
+        
+        for column_list in column_lists:
+            print(column_list)
+
+            for i in remaining_items1:
+                c1_row = contents1[i]
+
+                relevant_col1 = [c1_row[r] for r in column_list]
+
+                if not all(relevant_col1):
+                    continue
+
+                matchings_for_i = []
+
+                for j in remaining_items2:
+                    c2_row = contents2[j]
+
+                    relevant_col2 = [c2_row[r] for r in column_list]
+                    
+                    if not all(relevant_col2):
+                        continue
+
+                    if relevant_col1 == relevant_col2:    
+                        matchings_for_i.append(j)
+
+                if len(matchings_for_i):
+                    matchings[i] += matchings_for_i
+                    temp_matched_items1.add(i)
+                    temp_matched_items2.update(matchings_for_i)
+                    
+        total_matched_items1.update(temp_matched_items1)
+        total_matched_items2.update(temp_matched_items2)
+
+    return (matchings, total_matched_items1, total_matched_items2)
