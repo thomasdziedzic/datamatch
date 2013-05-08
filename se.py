ecabac25b3028068f54f48ba5914119c87256929
Thomas Dziedzic <gostrc@gmail.com>
2013-05-07 23:44:05 -0700

    %s/reader\(1|2\)/content\1/g in se.py

--- se.py
+++ se.py
@@ -4,15 +4,15 @@ import utilities
 
 MIN_COLUMNS_TO_MATCH = 2
 
-reader1 = utilities.lowerCSV(utilities.readCSV('1.csv'))
-reader2 = utilities.lowerCSV(utilities.readCSV('2.csv'))
+contents1 = utilities.lowerCSV(utilities.readCSV('1.csv'))
+contents2 = utilities.lowerCSV(utilities.readCSV('2.csv'))
 
-matchings, total_matched_items1, total_matched_items2 = utilities.match_data(reader1, reader2, MIN_COLUMNS_TO_MATCH)
+matchings, total_matched_items1, total_matched_items2 = utilities.match_data(contents1, contents2, MIN_COLUMNS_TO_MATCH)
 
 print(matchings)
 print(len(matchings))
 
-utilities.write_matches_csv('machings.csv', matchings, reader1, reader2)
+utilities.write_matches_csv('machings.csv', matchings, contents1, contents2)
 
-utilities.write_no_matches_csv('nomatches1.csv', reader1, total_matched_items1)
-utilities.write_no_matches_csv('nomatches2.csv', reader2, total_matched_items2)
+utilities.write_no_matches_csv('nomatches1.csv', contents1, total_matched_items1)
+utilities.write_no_matches_csv('nomatches2.csv', contents2, total_matched_items2)
